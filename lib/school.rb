20d13dee158cf28347993c8d0447ee3c03cee2de
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-22 13:54:55 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,14 +4,17 @@ class School
     @school_name = school_name 
     @roster = {}
   end
-  # def name 
-  #   @name 
-  # end 
+  
   def roster
     @roster
   end 
+  
   def add_student(name, grade)
     @roster[grade] = []
     @roster[grade] << name 
   end 
+  
 end 
+
+
+
