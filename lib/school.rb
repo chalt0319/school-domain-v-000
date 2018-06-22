ea39ba4e4640b681d22365c84b43ccb87aab0bee
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-22 14:12:30 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,7 @@ class School
   end 
   
   def add_student(name, grade)
-    if @roster[grade] == grade 
+    if @roster == grade 
     @roster[grade] = []
     @roster[grade] << name 
     # binding.pry
