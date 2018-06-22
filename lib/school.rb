fc1ebae20440eeacf1e6ad79600f3b92c0f9fc0e
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-22 13:47:07 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,7 @@
 # code here!
 class School
-  def initialize(name)
-    @name = name 
+  def initialize(school_name)
+    @school_name = school_name 
     @roster = {}
   end
   def name 
@@ -10,4 +10,5 @@ class School
   def roster
     @roster
   end 
+  def add_student()
 end 
